cbd1c8f16ee3ba3c4c4cc624c5698980c412c23f
Ronald Pereira <rpereira@taecontrol.com>
2025-02-18 14:01:24 -0400

    update: rename magic link

--- resources/views/filament/pages/auth/login.blade.php
+++ resources/views/filament/pages/auth/login.blade.php
@@ -3,30 +3,39 @@
 @section('title', 'Iniciar sesión')
 
 @section('content')
-<div class="flex items-center justify-center min-h-screen bg-gray-100">
-    <div class="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
-        <h2 class="text-2xl font-bold text-center">Iniciar sesión</h2>
+  <div class="flex min-h-screen items-center justify-center bg-gray-100">
+    <div class="w-full max-w-md space-y-8 rounded-lg bg-white p-8 shadow-md">
+      <h2 class="text-center text-2xl font-bold">Iniciar sesión</h2>
 
-        @if (session('message'))
-            <div class="mt-4 p-2 bg-green-100 text-green-700 rounded">
-                {{ session('message') }}
-            </div>
-        @endif
-
-        <form wire:submit.prevent="submit" method="POST">
-            {{ $this->form }}
-
-            <div class="mt-6">
-                <button type="submit" class="bg-indigo-500 text-white py-2 px-4 rounded w-full">
-                    Enviar enlace mágico
-                </button>
-            </div>
-        </form>
-
-        <div class="mt-4 text-center text-sm text-gray-600">
-            Lea los términos y condiciones de uso <a href="https://mamapay.test" target="_blank"
-                class="underline text-primary-600 hover:text-primary-500">aquí</a>
+      @if (session('message'))
+        <div class="mt-4 rounded bg-green-100 p-2 text-green-700">
+          {{ session('message') }}
         </div>
+      @endif
+
+      <form
+        wire:submit.prevent="submit"
+        method="POST"
+      >
+        {{ $this->form }}
+
+        <div class="mt-6">
+          <button
+            type="submit"
+            class="w-full rounded bg-indigo-500 px-4 py-2 text-white"
+          >
+            Enviar Enlace
+          </button>
+        </div>
+      </form>
+
+      <div class="mt-4 text-center text-sm text-gray-600">
+        Lea los términos y condiciones de uso <a
+          href="https://mamapay.test"
+          target="_blank"
+          class="text-primary-600 hover:text-primary-500 underline"
+        >aquí</a>
+      </div>
     </div>
-</div>
+  </div>
 @endsection
