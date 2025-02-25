41e5421bdca7ec1da740c048fdf74259fcc5e188
Ronald Pereira <rpereira@taecontrol.com>
2025-02-25 14:13:05 -0400

    fix: BankAccounts Resources (App and Store)

--- app/Filament/App/Resources/BankAccountResource/Pages/CreateBankAccount.php
+++ app/Filament/App/Resources/BankAccountResource/Pages/CreateBankAccount.php
@@ -12,42 +12,18 @@ class CreateBankAccount extends CreateRecord
 
     protected function mutateFormDataBeforeCreate(array $data): array
     {
-        $user = auth()->user(); // Obtener el usuario autenticado
-
-        // Asignar el ID del usuario autenticado
-        $data['user_id'] = $user->id;
-
-        // Obtener la cédula del usuario y remover el guion
-        $data['identity_number'] = str_replace('-', '', $user->identity_document);
-
-        // Concatenar el prefijo telefónico y el número telefónico
-        $data['phone_number'] = $data['phone_prefix'].$data['phone_number'];
-
-        // Eliminar el campo `phone_prefix` ya que no es necesario almacenarlo por separado
-        unset($data['phone_prefix']);
-
-        // Verificar si el usuario ya tiene una cuenta predeterminada
-        $hasDefaultAccount = BankAccount::where('user_id', $user->id)
-            ->where('default_account', true)
-            ->exists();
-
-        // Si no tiene una cuenta predeterminada, marcar esta como predeterminada
-        $data['default_account'] = ! $hasDefaultAccount;
+        $data['user_id'] = auth()->id();
 
         return $data;
     }
 
     protected function afterCreate(): void
     {
-        $user = auth()->user(); // Obtener el usuario autenticado
+        $user = auth()->user();
 
-        // Asegurar que solo una cuenta sea predeterminada
-        $createdAccount = $this->record; // Obtener la cuenta recién creada
-
-        if ($createdAccount->default_account) {
-            // Desmarcar otras cuentas como predeterminadas
+        if ($this->record->default_account) {
             BankAccount::where('user_id', $user->id)
-                ->where('id', '!=', $createdAccount->id)
+                ->where('id', '!=', $this->record->id)
                 ->update(['default_account' => false]);
         }
     }
--- app/Filament/Store/Resources/BankAccountResource/Pages/EditBankAccount.php
+++ app/Filament/Store/Resources/BankAccountResource/Pages/EditBankAccount.php
@@ -3,7 +3,9 @@
 namespace App\Filament\Store\Resources\BankAccountResource\Pages;
 
 use App\Filament\Store\Resources\BankAccountResource;
+use App\Models\BankAccount;
 use Filament\Actions;
+use Filament\Facades\Filament;
 use Filament\Resources\Pages\EditRecord;
 
 class EditBankAccount extends EditRecord
@@ -15,5 +17,16 @@ class EditBankAccount extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+
+    protected function afterSave(): void
+    {
+        $store = Filament::getTenant();
+
+        if ($this->record->default_account) {
+            BankAccount::where('store_id', $store->id)
+                ->where('id', '!=', $this->record->id)
+                ->update(['default_account' => false]);
+        }
     }
 }
